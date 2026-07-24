ae60ee28daba05fcc046b6f99ea63d3850695ade
Luis Gomes <luisgomesbcc@gmail.com>
2026-07-24 18:40:53 -0300

    Addicionando analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
 import { Stack } from 'expo-router'
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+      <Analytics />
+    </>
+  )
 }
